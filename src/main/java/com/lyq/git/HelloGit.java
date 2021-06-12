ea369a376bd49c23f6f884f5ac7b37602e2d461d
luyanqaing <936601272@qq.com>
2021-06-12 19:24:30 +0800

    分支操作

--- src/main/java/com/lyq/git/HelloGit.java
+++ src/main/java/com/lyq/git/HelloGit.java
@@ -5,6 +5,9 @@ public class HelloGit {
 		System.out.println("Hello Git!");
 		System.out.println("更新操作1");
 		System.out.println("更新操作2");
+		
+		System.out.println("分支操作1");
+		System.out.println("分支操作2");
 	}
 
 }
