8cfc777bb0e9d38aec4bc7822c5b1d70d7a14b74
luyanqaing <936601272@qq.com>
2021-06-12 19:20:51 +0800

    更新操作2

--- src/main/java/com/lyq/git/HelloGit.java
+++ src/main/java/com/lyq/git/HelloGit.java
@@ -3,6 +3,8 @@ package com.lyq.git;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
+		System.out.println("更新操作1");
+		System.out.println("更新操作2");
 	}
 
 }
